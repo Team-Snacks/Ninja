3a81d56af6028fddf0a9beb691587d98fdb0155f
scarf <greenscarf005@gmail.com>
2022-10-21 14:06:31 +0900

    refactor: 사용하지 않는 파일 정리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import GridLayout from 'react-grid-layout'
-import '/node_modules/react-grid-layout/css/styles.css'
-import '/node_modules/react-resizable/css/styles.css'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
 
 function App() {
   const layout = [
