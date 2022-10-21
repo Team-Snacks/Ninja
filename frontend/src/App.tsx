7095a1c8ab99a1f8e4947395e4d6ba77077be513
scarf <greenscarf005@gmail.com>
2022-10-21 15:02:39 +0900

    feat: 목업 제작

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,41 @@
-import { Grid, NavLink } from '@mantine/core'
+import { AppShell, Navbar, Header, Grid, NavLink, Title } from '@mantine/core'
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
-import {
-  BrowserRouter,
-  Link,
-  Route,
-  Routes,
-  useLocation,
-} from 'react-router-dom'
-import { CreateAccount, Login } from './Login'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { Register, Login } from './Login'
+import { paths } from './paths'
 import { WidgetGrid } from './WidgetGrid'
 
-export const Root = () => {
-  const location = useLocation()
-
+export const Router = () => {
   return (
-    <>
-      <NavLink
-        label="로그인"
-        component={Link}
-        to="/user/login"
-        active={location.pathname === '/user/login'}
-      />
-      <NavLink
-        label="회원가입"
-        component={Link}
-        to="/user/login"
-        active={location.pathname === '/user'}
-      />
-      <WidgetGrid />
-    </>
+    <Routes>
+      <Route path={paths.home} element={<WidgetGrid />} />
+      <Route path={paths.register} element={<Register />} />
+      <Route path={paths.login} element={<Login />} />
+    </Routes>
   )
 }
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Root />} />
-        <Route path="/user" element={<CreateAccount />} />
-        <Route path="/user/login" element={<Login />} />
-      </Routes>
+      <AppShell
+        padding="md"
+        navbar={
+          <Navbar width={{ base: 300 }} height={500} p="xs">
+            <Link to={paths.home}>홈</Link>
+            <Link to={paths.register}>회원가입</Link>
+            <Link to={paths.login}>로그인</Link>
+          </Navbar>
+        }
+        header={
+          <Header height={60} p="xs">
+            <Title>🍪 Snacks (Ninja)</Title>
+          </Header>
+        }
+      >
+        <Router />
+      </AppShell>
     </BrowserRouter>
   )
 }
--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,4 +1,6 @@
-import { Button, Stack, TextInput, Title } from '@mantine/core'
+import { Button, Stack, Text, TextInput, Title } from '@mantine/core'
+import { Link } from 'react-router-dom'
+import { paths } from './paths'
 
 export const Login = () => {
   return (
@@ -7,18 +9,23 @@ export const Login = () => {
       <TextInput placeholder="example@gmail.com" label="이메일" withAsterisk />
       <TextInput placeholder="****" label="비밀번호" withAsterisk />
       <Button>로그인</Button>
-      <Button>회원가입</Button>
+      <Link to={paths.register}>
+        <Text>회원가입</Text>
+      </Link>
     </Stack>
   )
 }
 
-export const CreateAccount = () => {
+export const Register = () => {
   return (
     <Stack>
       <Title>회원가입</Title>
       <TextInput placeholder="example@gmail.com" label="이메일" withAsterisk />
       <TextInput placeholder="****" label="비밀번호" withAsterisk />
       <Button>회원가입</Button>
+      <Link to={paths.login}>
+        <Text>로그인</Text>
+      </Link>
     </Stack>
   )
 }
